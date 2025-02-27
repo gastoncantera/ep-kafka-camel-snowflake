d9829279550da58e60513f5ad7fd138e9c1a664a
Gaston Cantera <65862015+gastoncantera@users.noreply.github.com>
2025-02-27 13:41:40 -0300

    Fix kamelet:snowflake-sink in KafkaCamelService configuration

--- src/main/java/com/example/KafkaCamelService.java
+++ src/main/java/com/example/KafkaCamelService.java
@@ -6,6 +6,9 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KafkaCamelService {
     private final CamelContext camelContext = new DefaultCamelContext();
 
@@ -30,7 +33,7 @@ public class KafkaCamelService {
 
         @Override
         public void configure() {
-            from("kafka:snowflake-topic?brokers=localhost:9092")
+            from("kafka:snowflake-topic?brokers=localhost:9092&groupId=ep-kafka-camel-snowflake")
 
                     // Snowpipe Streaming: Snowflake Ingest Service Java SDK
                     // https://github.com/snowflakedb/snowflake-ingest-java
@@ -43,12 +46,12 @@ public class KafkaCamelService {
                     // Apache Camel Kafka Connector: Kamelet Snowflake Sink
                     // https://github.com/apache/camel-kafka-connector/tree/main/connectors/camel-snowflake-sink-kafka-connector
                     // https://camel.apache.org/camel-kafka-connector/4.8.x/reference/connectors/camel-snowflake-sink-kafka-sink-connector.html
-                    .to("kamelet:snowflake-sink"
+                    .toD("kamelet:snowflake-sink"
                             + "?instanceUrl=http://snowflake.localhost.localstack.cloud:4566"
                             + "&username=test"
                             + "&password=test"
                             + "&databaseName=test"
-                            + "&query=INSERT INTO public.\"kafka-streaming\" (RECORD_CONTENT, RECORD_METADATA) VALUES ('FIXME', 'kamelet:snowflake-sink')"
+                            + "&query=INSERT INTO public.\"kafka-streaming\" (RECORD_CONTENT,RECORD_METADATA) VALUES ('${body}','${headers}')"
                     )
 
                     .log(LoggingLevel.INFO, "Message processed and sent to Snowflake");
